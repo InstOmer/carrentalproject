acb4de6e21fe228e1e26fcb990a78f28fd80b2c1
InstOmer <ogokdemir.techproed@gmail.com>
2024-09-18 21:59:24 +0300

    CarRentalProject_v01

---
+++ cars/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from cars.models import Car
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ["id", "model", "transmission", "airConditioning", "pricePerHour", "fuelType"]
+    list_display_links = ["model"]
